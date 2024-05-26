9651683dcb9ceaa12701a7de5fda8522b55c414c
muhammad_Sarahni <117571362+muhammadser1@users.noreply.github.com>
2024-05-26 19:04:12 +0300

    Connect pythoncode to MySql apache and test it

--- models/mysql_database.py
+++ models/mysql_database.py
@@ -1,9 +1,26 @@
-import database
+from models.database import Database
+import mysql.connector
 
-class Mysql_database(database):
+
+class Mysql_database(Database):
     def __init__(self):
-        self.config_db
-        self.connect
+        self.config = {
+            'user': 'root',
+            'password': '',
+            'host': 'localhost',
+            'port': '3306',
+            'database': 'pokemons'
+        }
+        self.connetion= self.connect()
+
+
+    def connect(self):
+        mydb = mysql.connector.connect(**self.config)
+        return mydb
+
+
+
+
 
     def config_db(self):
         pass
@@ -12,4 +29,22 @@ class Mysql_database(database):
         pass
 
     def add_pokemon(self,data):
+        pass
+
+    def get_pokemon_by_type(self,type):
+        pass
+
+    def get_pokemon_by_trainer(self,trainer):
+        pass
+
+    def get_trainers_of_pokemon(self,pokemon):
+        pass
+
+    def delete_pokemon(self,pokemon):
+        pass
+
+    def add_pokemon_to_trainer(self, pokemon, trainer):
+        pass
+
+    def evolve(self, pokemon):
         pass
--- server.py
+++ server.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
+from models import mysql_database
+
+mySql=mysql_database.Mysql_database()
 server = FastAPI()
 
 #test the server is up
 @server.get("/test")
 def test():
+
+    cursor = mySql.connetion.cursor()
+    cursor.execute("SELECT * FROM test")
+    result = cursor.fetchall()
+    print(result)
     return "The server is working properly!"
